25c77351cafcec4719f6ee3487b7a55d53fa43be
chloemarion <lacymendes98@gmail.com>
2018-08-07 20:43:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 def fizzbuzz(number)
   if number % 3 && number % 5 == 0
   return "FizzBuzz"
-  else number % 3 == 0 
+  elsif number % 3 == 0 
   return "Fizz"
-else number % 5 == 0 
+elsif number % 5 == 0 
   return "Buzz"
 else
   return "Nil"
